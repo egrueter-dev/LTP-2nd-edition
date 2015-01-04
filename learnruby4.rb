ab0916d754db15fe79f56f3c4a1112a39b728ca2
Erik Grueter <erikgrueter@gmail.com>
2015-01-04 18:49:29 +0100

    more exercises from chapter 4

---
+++ learnruby4.rb
@@ -0,0 +1,21 @@
+# favorites = []
+# favorites.push 'raindrops'
+# favorites.push 'kittens'
+# favorites.pop
+# puts favorites
+# puts favorites.length
+
+
+#Array Challenges
+
+#1 Alphabetical order
+array = []
+
+while
+  input = gets.chomp.to_s
+  array << input
+  if input == ''
+    puts array.sort
+    break
+  end
+end
