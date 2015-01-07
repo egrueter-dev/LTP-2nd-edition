9f435084921a5d73dd24e499d8682da7ac364faf
Erik Grueter <erikgrueter@gmail.com>
2015-01-07 17:37:39 +0100

    more recursion & array sorting practice

--- learnruby7.rb
+++ learnruby7.rb
@@ -38,12 +38,82 @@
 # ask_recursively "Does this work?"
 #
 
-def factorial(num)
-  if num <= 1
-    1
+# def factorial(num)
+#   if num >= 2
+#     num * factorial(num-1)
+#   else
+#     1
+#   end
+# end
+#
+# puts factorial(10)
+#
+# world = [
+# [o,o,o,o,o,o,o,o,o,o,o],
+# [o,o,o,o,M,M,o,o,o,o,o],
+# [o,o,o,o,o,o,o,o,M,M,o],
+# [o,o,o,M,o,o,o,o,o,M,o],
+# [o,o,o,M,o,M,M,o,o,o,o],
+# [o,o,o,o,M,M,M,M,o,o,o],
+# [o,o,o,M,M,M,M,M,M,M,o],
+# [o,o,o,M,M,o,M,M,M,o,o],
+# [o,o,o,o,o,o,M,M,o,o,o],
+# [o,M,o,o,o,M,o,o,o,o,o],
+# ]
+#
+#
+# def continent_size(world, y, x)
+#   if world[y] [x] != 'land'
+#     return 0
+#   end
+#   size = 1
+#   world[y] [x] = 'counted land'
+#
+#   size = size + continent_size(world, x-1, y-1)
+#   size = size + continent_size(world, x,   y-1)
+#   size = size + continent_size(world, x+1, y-1)
+#   size = size + continent_size(world, x+1, y)
+#   size = size + continent_size(world, x-1, y)
+#   size = size + continent_size(world, x-1, y+1)
+#   size = size + continent_size(world, x,   y+1)
+#   size = size + continent_size(world, x+1, y+1)
+#
+#   size
+# end
+#
+#
+# puts continent_size(world, 5, 5)
+
+
+#<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<#Sorting solution
+
+
+def sort arr
+  rec_sort arr, []
+end
+
+
+def rec_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+smallest = unsorted.pop
+still_unsorted = []
+
+unsorted.each do |tested_object|
+  if tested_object < smallest
+    still_unsorted.push smallest
+    smallest = tested_object
   else
-    num * factorial(num-1)
+    still_unsorted.push tested_object
   end
 end
 
-puts factorial(10)
+sorted.push smallest
+
+rec_sort still_unsorted, sorted
+
+end
+
+puts(sort(['can','feel','singing','like','a','can']))
